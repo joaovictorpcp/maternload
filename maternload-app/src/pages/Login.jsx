cc917c085a9712f19786f42453b72c8f990a9a14
João Victor Pinheiro <joaovictorpinheirocp@gmail.com>
2026-04-04 12:20:01 -0300

    agora vai, login

--- maternload-app/src/pages/Login.jsx
+++ maternload-app/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import { Heart, FlaskConical } from 'lucide-react'
